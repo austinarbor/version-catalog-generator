5a37dc504496465397d97fc98571cb36097c3e50
Austin Arbor <aarbor989@gmail.com>
2023-08-17 01:15:22 -0400

    add initial implementation of CatalogParser

---
+++ plugin/src/main/kotlin/dev/aga/gradle/plugin/versioncatalogs/toml/CatalogParser.kt
@@ -0,0 +1,63 @@
+package dev.aga.gradle.plugin.versioncatalogs.toml
+
+import org.apache.maven.model.Dependency
+import org.tomlj.Toml
+import org.tomlj.TomlParseResult
+import org.tomlj.TomlTable
+import java.io.File
+
+object CatalogParser {
+    fun findBom(file: File, name: String): Dependency {
+        val parsed = parseCatalog(file)
+        return findBom(parsed, name)
+    }
+
+    private fun findBom(toml: TomlParseResult, libraryName: String): Dependency {
+        val library =
+            toml["libraries"]
+                ?.let { it as? TomlTable }
+                ?.let { it[libraryName] }
+                ?.let { it as? TomlTable }
+                ?: throw RuntimeException("${libraryName} not found in catalog file")
+
+        val versions =
+            toml["versions"]
+                ?.let { it as? TomlTable }
+
+        return getGAV(library, versions)
+    }
+
+    private fun getGAV(library: TomlTable, versions: TomlTable?): Dependency {
+        val (group, name) =
+            if (library["module"] is String) {
+                val split = (library["module"] as String).split(":")
+                split[0] to split[1]
+            } else {
+                val group = library["group"]?.let { it as? String } ?: throw RuntimeException("Group not found ")
+                val name = library["name"]?.let { it as? String } ?: throw RuntimeException("Name not found")
+                val version = getVersion(library, versions)
+                group to name
+            }
+
+        val version = getVersion(library, versions)
+        return Dependency().apply {
+            groupId = group
+            artifactId = name
+            this.version = version
+        }
+
+    }
+
+    private fun getVersion(library: TomlTable, versions: TomlTable?): String {
+        library.getString("version.ref")
+            ?.let {
+                return versions?.getString(it) ?: throw RuntimeException("Version ref '${it}' not found")
+            }
+
+        return library.getString("version") ?: throw RuntimeException("Version not found")
+    }
+
+    private fun parseCatalog(file: File): TomlParseResult {
+        return Toml.parse(file.toPath())
+    }
+}
---
+++ plugin/src/test/kotlin/dev/aga/gradle/plugin/versioncatalogs/toml/CatalogParserTest.kt
@@ -0,0 +1,49 @@
+package dev.aga.gradle.plugin.versioncatalogs.toml
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.nio.file.Path
+import java.nio.file.Paths
+
+internal class CatalogParserTest {
+    @ParameterizedTest
+    @MethodSource("testFindBomProvider")
+    fun testFindBom(libraryName: String, expected: Array<String>, shouldThrow: Boolean = false) {
+        val file = buildPath("libs.versions.toml").toFile()
+        if (!shouldThrow) {
+            val actual = CatalogParser.findBom(file, libraryName)
+            assertThat(actual)
+                .extracting("groupId", "artifactId", "version")
+                .containsExactly(*expected)
+        } else {
+            assertThatExceptionOfType(RuntimeException::class.java)
+                .isThrownBy { CatalogParser.findBom(file, libraryName) }
+        }
+    }
+
+    companion object {
+
+        private val srcDir = "src/test/resources"
+
+        @JvmStatic
+        private fun testFindBomProvider(): List<Arguments> {
+            return listOf(
+                arguments("groovy-core", arrayOf("org.codehaus.groovy", "groovy", "3.0.5"), false),
+                arguments("fake-lib", arrayOf("dev.aga.lib", "fake-lib", "1.0.2"), false),
+                arguments("another-lib", arrayOf("dev.aga.lib", "another-lib", "1.0.0"), false),
+                arguments("commons-lang3", arrayOf(""), true)
+            )
+        }
+
+        private fun buildPath(fileName: String): Path {
+            return Paths.get(srcDir, fileName)
+        }
+
+    }
+
+
+}
