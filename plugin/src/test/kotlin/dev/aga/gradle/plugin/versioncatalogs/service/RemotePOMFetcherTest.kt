8f3d2e55c6098fb7d17e295de7f0f0d6f9182cfd
Austin Arbor <aarbor989@gmail.com>
2023-08-26 17:35:24 -0400

    add test for Gradle dependencies with RemotePOMFetcher

--- plugin/src/test/kotlin/dev/aga/gradle/plugin/versioncatalogs/service/RemotePOMFetcherTest.kt
+++ plugin/src/test/kotlin/dev/aga/gradle/plugin/versioncatalogs/service/RemotePOMFetcherTest.kt
@@ -3,6 +3,7 @@ package dev.aga.gradle.plugin.versioncatalogs.service
 import org.apache.maven.model.Dependency
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
 import org.junit.jupiter.api.Test
 
 internal class RemotePOMFetcherTest {
@@ -28,6 +29,21 @@ internal class RemotePOMFetcherTest {
                 artifactId = "spring-boot-dependencies"
                 version = "3.1.2"
             }
+        val result = fetcher.fetch(dep)
+        assertThat(result.groupId).isEqualTo("org.springframework.boot")
+        assertThat(result.artifactId).isEqualTo("spring-boot-dependencies")
+        assertThat(result.version).isEqualTo("3.1.2")
+        assertThat(result.dependencyManagement.dependencies).hasSize(398)
+    }
+
+    @Test
+    fun testFetch_GradleDependency() {
+        val dep =
+            DefaultExternalModuleDependency(
+                "org.springframework.boot",
+                "spring-boot-dependencies",
+                "3.1.2",
+            )
         val result = fetcher.fetch(dep)
         assertThat(result.groupId).isEqualTo("org.springframework.boot")
         assertThat(result.artifactId).isEqualTo("spring-boot-dependencies")
