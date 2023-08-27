955e9591d62637ebc72ff4c471dfd5e8c1b95b36
Austin Arbor <aarbor989@gmail.com>
2023-08-27 13:07:29 -0400

    verify first fetcher in chain is used

--- plugin/src/test/kotlin/dev/aga/gradle/plugin/versioncatalogs/service/POMFetcherChainTest.kt
+++ plugin/src/test/kotlin/dev/aga/gradle/plugin/versioncatalogs/service/POMFetcherChainTest.kt
@@ -3,6 +3,8 @@ package dev.aga.gradle.plugin.versioncatalogs.service
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verifyNoInteractions
 
 internal class POMFetcherChainTest {
     @Test
@@ -14,6 +16,19 @@ internal class POMFetcherChainTest {
             )
         val model = chain.fetch("org.assertj", "assertj-bom", "3.24.2")
         assertThat(model.artifactId).isEqualTo("assertj-bom")
+    }
+
+    @Test
+    fun testFetch_UsesFirst() {
+        val mockFetcher = mock<POMFetcher>()
+        val chain =
+            POMFetcherChain(
+                LocalPOMFetcher("src/test/resources/poms"),
+                mockFetcher,
+            )
+        val model = chain.fetch("org.assertj", "assertj-bom", "3.24.2")
+        assertThat(model.artifactId).isEqualTo("assertj-bom")
+        verifyNoInteractions(mockFetcher)
     }
 
     @Test
