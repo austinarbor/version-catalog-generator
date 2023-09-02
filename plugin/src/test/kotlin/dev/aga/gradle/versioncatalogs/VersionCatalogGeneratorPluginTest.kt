7c1760d43c81b17378148c8d56bf2a4a4849036c
Austin Arbor <aarbor989@gmail.com>
2023-09-02 10:05:27 -0400

    add test for groovy dsl

--- plugin/src/test/kotlin/dev/aga/gradle/versioncatalogs/VersionCatalogGeneratorPluginTest.kt
+++ plugin/src/test/kotlin/dev/aga/gradle/versioncatalogs/VersionCatalogGeneratorPluginTest.kt
@@ -12,6 +12,7 @@ class VersionCatalogGeneratorPluginTest {
 
     private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
     private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
+    private val groovySettingsFile by lazy { projectDir.resolve("settings.gradle") }
     private val propertiesFile by lazy { projectDir.resolve("gradle.properties") }
 
     private lateinit var classpathString: String
@@ -30,7 +31,7 @@ class VersionCatalogGeneratorPluginTest {
     }
 
     @Test
-    fun `plugin usage succeeds`() {
+    fun `kotlin dsl usage succeeds`() {
         // Set up the test build
         settingsFile.writeText(
             """
@@ -50,6 +51,58 @@ class VersionCatalogGeneratorPluginTest {
               versionCatalogs {
                 generate("jsonLibs") {
                   from("com.fasterxml.jackson:jackson-bom:2.15.2")
+                }
+              }
+            }
+        """
+                .trimIndent(),
+        )
+        buildFile.writeText(
+            """
+            plugins {
+              java
+            }
+            dependencies {
+              implementation(jsonLibs.core.jackson.databind)
+              implementation(jsonLibs.bundles.jackson.module)
+            }
+            """
+                .trimIndent(),
+        )
+
+        // Run the build
+        val runner =
+            GradleRunner.create().forwardOutput().withPluginClasspath().withProjectDir(projectDir)
+
+        val result = runner.build()
+
+        assertThat(result.output).contains("BUILD SUCCESSFUL")
+    }
+
+    @Test
+    fun `groovy dsl usage succeeds`() {
+        // Set up the test build
+        groovySettingsFile.writeText(
+            """
+            import static dev.aga.gradle.versioncatalogs.Generator.INSTANCE as Generator
+            
+            buildscript {
+              dependencies {
+                classpath(files($classpathString))
+              }
+            }
+            plugins {
+                id("dev.aga.gradle.version-catalog-generator")
+            }
+            dependencyResolutionManagement {
+              repositories {
+                mavenCentral()
+              }
+              versionCatalogs {
+                Generator.generate(it, "jsonLibs") {
+                  it.from("com.fasterxml.jackson:jackson-bom:2.15.2")
+                  it.libraryAliasGenerator = dev.aga.gradle.versioncatalogs.GeneratorConfig.DEFAULT_ALIAS_GENERATOR
+                  it.versionNameGenerator = dev.aga.gradle.versioncatalogs.GeneratorConfig.DEFAULT_VERSION_NAME_GENERATOR
                 }
               }
             }
