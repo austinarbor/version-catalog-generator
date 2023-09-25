e871ddd46c3546ce3e88c8af66ce76267da6130f
Austin Arbor <aarbor989@gmail.com>
2023-09-24 22:57:13 -0400

    add static caseChange function

--- plugin/src/main/kotlin/dev/aga/gradle/versioncatalogs/VersionCatalogGeneratorPluginExtension.kt
+++ plugin/src/main/kotlin/dev/aga/gradle/versioncatalogs/VersionCatalogGeneratorPluginExtension.kt
@@ -4,6 +4,8 @@ import dev.aga.gradle.versioncatalogs.Generator.generate
 import dev.aga.gradle.versioncatalogs.service.FileCatalogParser
 import java.nio.file.Paths
 import javax.inject.Inject
+import net.pearx.kasechange.CaseFormat
+import net.pearx.kasechange.formatter.format
 import org.gradle.api.Action
 import org.gradle.api.initialization.Settings
 import org.gradle.api.initialization.dsl.VersionCatalogBuilder
@@ -240,6 +242,20 @@ constructor(
                 .replace(versionRegEx, "")
                 .replace(dotRegex, ".") // replace 2 or more consecutive periods with a single one
                 .trim('.') // trim leading and trailing periods
+        }
+
+        /**
+         * Function to easily change the case of a string from one format to another
+         *
+         * @param str the string to change the casing of
+         * @param from the starting format
+         * @param to the desired format
+         * @return the reformatted string
+         */
+        @JvmStatic
+        fun caseChange(str: String, from: CaseFormat, to: CaseFormat): String {
+            val split = from.splitToWords(str)
+            return to.format(split)
         }
     }
 }
