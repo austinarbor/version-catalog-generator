97082637ddb02ea6608e54b7941410832fd6a1e1
Austin Arbor <aarbor989@gmail.com>
2023-10-25 20:14:24 -0400

    update gradle plugin tags

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -74,7 +74,7 @@ gradlePlugin {
         implementationClass = "dev.aga.gradle.versioncatalogs.VersionCatalogGeneratorPlugin"
         displayName = PLUGIN_DISPLAY_NAME
         description = PLUGIN_DESCRIPTION
-        tags = listOf("version", "catalog", "bom", "generate", "pom")
+        tags = listOf("version", "catalog", "generate", "bom", "pom", "dependencies", "dependency-management")
     }
 }
 
