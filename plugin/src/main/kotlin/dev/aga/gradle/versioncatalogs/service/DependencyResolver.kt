8bfb623acdffec9d771b48a7de9f8df19dbb63ae
Austin Arbor <aarbor989@gmail.com>
2023-11-11 20:27:49 -0500

    remove unused function

--- plugin/src/main/kotlin/dev/aga/gradle/versioncatalogs/service/DependencyResolver.kt
+++ plugin/src/main/kotlin/dev/aga/gradle/versioncatalogs/service/DependencyResolver.kt
@@ -4,16 +4,5 @@ import org.apache.maven.model.Dependency
 import org.apache.maven.model.Model
 
 interface DependencyResolver {
-
     fun resolve(source: Dependency): Pair<Model, Model?>
-
-    fun resolve(groupId: String, artifactId: String, version: String): Pair<Model, Model?> {
-        val dep =
-            Dependency().apply {
-                this.groupId = groupId
-                this.artifactId = artifactId
-                this.version = version
-            }
-        return resolve(dep)
-    }
 }
