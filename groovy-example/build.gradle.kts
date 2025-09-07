f098c61e00a441562875cd8f515756bbdf7e5429
Austin Arbor <aarbor989@gmail.com>
2025-09-07 19:25:15 -0400

    enhance the groovy example project as well

--- groovy-example/build.gradle.kts
+++ groovy-example/build.gradle.kts
@@ -1,9 +1,21 @@
-plugins { java }
+plugins {
+  java
+  alias(myLibs.plugins.kotlin.jvm)
+}
 
 dependencies {
   implementation(springLibs.spring.springBootStarterWeb)
   implementation(jsonLibs.jackson.jacksonDatabind)
   implementation(jsonLibs.bundles.jacksonModule)
+  implementation(awsLibs.s3)
+  implementation(manyBoms.spring.springBootStarterJdbc)
+  implementation(manyBoms.sts)
+  implementation(manyBoms.jacksonDatabind)
+  compileOnly(libs.spring.boot.dependencies)
+  implementation(myLibs.sqs)
+  implementation(myLibs.bundles.merged)
   testImplementation(mockitoLibs.mockito.mockitoCore)
+  testImplementation(mockitoLibs.mockito.mockitoJunitJupiter)
   testImplementation(junitLibs.junitJupiter)
+  testImplementation(junitLibs.junitJupiterParams)
 }
